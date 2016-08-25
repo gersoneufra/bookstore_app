aabf14876f48b9a58fc421f431e626c14a69abd7
gersoneufra <gersoneufra@gmail.com>
2016-08-25 00:18:01 -0500

    implement authors controller show action

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe AuthorsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET #show' do
+    it "returns sucessfull a http request status code" do
+      author = Fabricate(:author)
+
+      get :show, id: author.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
