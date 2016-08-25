809127ebadd995758ca55c2d6a6f09b3da214f6b
gersoneufra <gersoneufra@gmail.com>
2016-08-25 01:05:16 -0500

    implement authors controller new action

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe AuthorsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET #new' do
+    it "returns sucessfull a http request status code" do
+      get :new
+
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
